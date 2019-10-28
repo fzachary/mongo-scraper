08603fbf876f706be139db5d72be3e6f2555b672
Frank Zachary <fzachary@gmail.com>
2019-10-28 12:41:15 -0400

    some js configuration work, added and styled navbar and parallax

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,23 @@
+// Dependencies
+const express = require('express');
 const router = require('express').Router();
+// const db = require('../models');
+
+module.exports = (db) => {
+    // Load home page
+    router.get('/', function (req, res) {
+        res.send('index')
+    });
+
+    // Load saved articles page
+    router.get('/saved', function (req, res) {
+        res.send('saved')
+    });
+
+    // 404 page for any unmatched results
+    router.get('*', function (req, res) {
+        res.send('404');
+    });
+
+    return router;
+};
--- server.js
+++ server.js
@@ -1,29 +1,46 @@
 // Dependencies
-var express = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+// Require the model for accessing the "article" collection
+// ....
 
 // Establish a PORT and Initialize Express
 var PORT = process.env.PORT || 3000;
+const app = express();
 
-var app = express();
+// Set up Morgan
+app.use(logger("dev"));
+// Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/mongoscraper ", { useNewUrlParser: true });
 
 // Handlebars
-const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// Controllers
-// ....
+// Simple index route
+app.get("/", function (req, res) {
+    res.render('index');
+})
 
 // Connect to MongoDB
 // ....
 
 // Mongoose
-// ....
+// var mongoose = require("mongoose");
 
 // Start the server
 app.listen(PORT, function() {
     console.log("Server listening at http://localhost:" + PORT);
 });
+
+module.exports = app;
